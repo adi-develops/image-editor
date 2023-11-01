98d09ed9de53e258f5a3ce25e7b5314cbb58ab1b
Aditya Agarwal <aditya.agarwal1311@gmail.com>
2023-11-02 00:45:06 +0530

    Added a few more operations

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 import cv2
 
@@ -15,31 +15,52 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def processImage(filename, operation):
-    print(f"File name is {filename} and operation is {operation}")
+    print(f"File name is {filename} and operation is {operation['operation']}")
     img = cv2.imread(f"uploads/{filename}")
-    match operation:
-        case "cgray":
-            imageProcessed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            newFileName = f"static/{filename}"
-            cv2.imwrite(newFileName, imageProcessed)
-            return newFileName
-        case "cwebp":
-            newFileName = f"static/{filename.split('.')[0]}.webp"
-            cv2.imwrite(newFileName, img)
-            return newFileName
-        case "cjpg":
-            newFileName = f"static/{filename.split('.')[0]}.jpg"
-            cv2.imwrite(newFileName, img)
-            return newFileName
-        case "cpng":
-            newFileName = f"static/{filename.split('.')[0]}.png"
-            cv2.imwrite(newFileName, img)
-            return newFileName
-        case "flip":
-            flippedImage = cv2.flip(img, -1)
-            newFileName = f"static/{filename}"
-            cv2.imwrite(newFileName, flippedImage)
-            return newFileName
+    print(operation)
+    match operation["operation"]:
+        case "transform":
+            match operation["transformType"]:
+                case "flip":
+                    match operation["flipType"]:
+                        case "vertical-flip":
+                            flippedImage = cv2.flip(img, 0)
+                            newFileName = filename
+                            cv2.imwrite(f"static/{newFileName}", flippedImage)
+                            return newFileName
+                        case "horizontal-flip":
+                            print("Horizontal Flip invoked")
+                            flippedImage = cv2.flip(img, 1)
+                            newFileName = filename
+                            cv2.imwrite(f"static/{newFileName}", flippedImage)
+                            return newFileName
+                        case "both-flip":
+                            flippedImage = cv2.flip(img, -1)
+                            newFileName = filename
+                            cv2.imwrite(f"static/{newFileName}", flippedImage)
+                            return newFileName
+        case "filter":
+            match operation["filterType"]:
+                case "cgray":
+                    imageProcessed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                    newFileName = filename
+                    cv2.imwrite(f"static/{newFileName}", imageProcessed)
+                    return newFileName
+        
+        case "convert":
+            match operation["convertType"]:
+                case "cwebp":
+                    newFileName = f"{filename.split('.')[0]}.webp"
+                    cv2.imwrite(f"static/{newFileName}", img)
+                    return newFileName
+                case "cjpg":
+                    newFileName = f"{filename.split('.')[0]}.jpg"
+                    cv2.imwrite(f"static/{newFileName}", img)
+                    return newFileName
+                case "cpng":
+                    newFileName = f"{filename.split('.')[0]}.png"
+                    cv2.imwrite(f"static/{newFileName}", img)
+                    return newFileName
 
 
 @app.route("/")
@@ -53,7 +74,7 @@ def about():
 @app.route("/edit", methods=["GET", "POST"])
 def edit():
     if request.method == "POST":
-        operation = request.form["operation"]
+        operation = request.form
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
@@ -68,7 +89,12 @@ def edit():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             new = processImage(filename, operation)
-            flash(f"Image has been processed and is available <a target='_blank' href='/{new}'>here</a>")
+            flash(new)
             return redirect(url_for('home'))
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    directory = 'static'
+    return send_from_directory(directory, filename, as_attachment=True)
 
 app.run(debug=True, port=8000)
